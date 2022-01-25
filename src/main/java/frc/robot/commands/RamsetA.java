167c1cf03e88b73a605b5788a758330fdf21a8dc
sammyredha <sammy.mounsif@melroseschools.com>
2022-01-24 20:06:50 -0500

    Funny little formatting changes because I forgot to build before I committed :)

--- src/main/java/frc/robot/commands/RamsetA.java
+++ src/main/java/frc/robot/commands/RamsetA.java
@@ -10,24 +10,23 @@ import frc.robot.subsystems.DriveSubsystem;
 
 public class RamsetA extends RamseteCommand {
 
-    protected Trajectory autoTrajectory;
+  protected Trajectory autoTrajectory;
 
-    public RamsetA(Trajectory autoTrajectory, DriveSubsystem driveSubsystem) {
-        super(
-            autoTrajectory,
-            driveSubsystem::getPose,
-            new RamseteController(
-                Constants.AutoConstants.RamseteB, Constants.AutoConstants.RamseteZeta),
-            new SimpleMotorFeedforward(
-                Constants.AutoConstants.ksVolts,
-                Constants.AutoConstants.ksVoltSecondsPerMeter,
-                Constants.AutoConstants.kaVoltSecondsSquaredPerMeter),
-            Constants.AutoConstants.kinematics,
-            driveSubsystem::getWheelSpeeds,
-            new PIDController(Constants.AutoConstants.kPDriveVel, 0, 0),
-            new PIDController(Constants.AutoConstants.kPDriveVel, 0, 0),
-            driveSubsystem::tankDriveVolts,
-            driveSubsystem);
-
-    }
+  public RamsetA(Trajectory autoTrajectory, DriveSubsystem driveSubsystem) {
+    super(
+        autoTrajectory,
+        driveSubsystem::getPose,
+        new RamseteController(
+            Constants.AutoConstants.RamseteB, Constants.AutoConstants.RamseteZeta),
+        new SimpleMotorFeedforward(
+            Constants.AutoConstants.ksVolts,
+            Constants.AutoConstants.ksVoltSecondsPerMeter,
+            Constants.AutoConstants.kaVoltSecondsSquaredPerMeter),
+        Constants.AutoConstants.kinematics,
+        driveSubsystem::getWheelSpeeds,
+        new PIDController(Constants.AutoConstants.kPDriveVel, 0, 0),
+        new PIDController(Constants.AutoConstants.kPDriveVel, 0, 0),
+        driveSubsystem::tankDriveVolts,
+        driveSubsystem);
+  }
 }
