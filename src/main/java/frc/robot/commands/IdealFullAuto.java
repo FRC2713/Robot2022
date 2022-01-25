5dee24f1c7b5117441768ad0f8df554ef34cef02
sammyredha <sammy.mounsif@melroseschools.com>
2022-01-24 20:15:52 -0500

    Funny little ideal auto path which will eventually also have intake stuff once intake is implemented :)

--- src/main/java/frc/robot/commands/IdealFullAuto.java
+++ src/main/java/frc/robot/commands/IdealFullAuto.java
@@ -1,11 +1,28 @@
 package frc.robot.commands;
 
+import com.pathplanner.lib.PathPlanner;
+import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.ShootSubsystem;
 
 public class IdealFullAuto extends SequentialCommandGroup {
+  Trajectory leg1 =
+      PathPlanner.loadPath(
+          "Leg1", Constants.AutoConstants.maxSpeed, Constants.AutoConstants.maxAccel);
+  Trajectory leg2 =
+      PathPlanner.loadPath(
+          "Leg2", Constants.AutoConstants.maxSpeed, Constants.AutoConstants.maxAccel);
+
   public IdealFullAuto(ShootSubsystem shootSubsystem, DriveSubsystem driveSubsystem) {
-    // just a pipe dream for now
+    addCommands(
+        // intake to full speed
+        new RamsetA(leg1, driveSubsystem),
+        new ShootALowBall(shootSubsystem),
+        new RamsetA(leg2, driveSubsystem),
+        new ShootALowBall(shootSubsystem)
+        // intake back to 0
+        );
   }
 }
