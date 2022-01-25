f98a883c4abc830747b6469a6e3ea76a35a9b01e
sammyredha <sammy.mounsif@melroseschools.com>
2022-01-24 20:03:56 -0500

    Funny little auto command that lets you stop in the middle to fire balls (without actually firing balls, yet)

---
+++ src/main/java/frc/robot/commands/IdealFullAuto.java
@@ -0,0 +1,11 @@
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.ShootSubsystem;
+
+public class IdealFullAuto extends SequentialCommandGroup{
+    public IdealFullAuto(ShootSubsystem shootSubsystem, DriveSubsystem driveSubsystem) {
+        //just a pipe dream for now
+    }
+}
