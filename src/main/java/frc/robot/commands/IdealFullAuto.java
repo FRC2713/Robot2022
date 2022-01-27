ea36b5ffefb312dfbe93c79a0eed68336bd308ea
sammyredha <sammy.mounsif@melroseschools.com>
2022-01-26 19:06:33 -0500

    Worked out some vestiges of the merge, added awesome new intake stop/start commands to the Ideal Full Auto (angelic chorus), which, by the way, are now in command groups to deploy and stash them (with placeholder variables due to we don't know what they're actually supposed to be)

--- src/main/java/frc/robot/commands/IdealFullAuto.java
+++ src/main/java/frc/robot/commands/IdealFullAuto.java
@@ -5,6 +5,8 @@ import edu.wpi.first.math.trajectory.Trajectory;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.Constants;
 import frc.robot.subsystems.DriveSubsystem;
+import frc.robot.subsystems.IntakeFourBar;
+import frc.robot.subsystems.IntakeSubsystem;
 import frc.robot.subsystems.ShootSubsystem;
 
 public class IdealFullAuto extends SequentialCommandGroup {
@@ -15,14 +17,15 @@ public class IdealFullAuto extends SequentialCommandGroup {
       PathPlanner.loadPath(
           "Leg2", Constants.AutoConstants.maxSpeed, Constants.AutoConstants.maxAccel);
 
-  public IdealFullAuto(ShootSubsystem shootSubsystem, DriveSubsystem driveSubsystem) {
+  public IdealFullAuto(ShootSubsystem shootSubsystem, DriveSubsystem driveSubsystem, IntakeSubsystem intakeSubsystem, IntakeFourBar intakeFourBar) {
     addCommands(
-        // intake to full speed
-        new RamsetA(leg1, driveSubsystem),
-        new ShootALowBall(shootSubsystem),
-        new RamsetA(leg2, driveSubsystem),
-        new ShootALowBall(shootSubsystem)
-        // intake back to 0
+        new DeployIntake(intakeSubsystem, intakeFourBar), //deploys intake
+        //command that makes the snek schmoove
+        new RamsetA(leg1, driveSubsystem), //first leg of the journey, ideally picks up a ball on the way
+        new ShootALowBall(shootSubsystem), //shoots until the ball goes by, in theory; will need two of these eventually for two balls
+        new RamsetA(leg2, driveSubsystem), //second leg of the journey, should pick up two balls on the way
+        new ShootALowBall(shootSubsystem), //shoots until the ball goes by, in theory; will need two of these eventually for two balls
+        new IntakeSetRollers(intakeSubsystem, 0) //turns off the rollers
         );
   }
 }
