fdaae24bb07aeb70d05de0090dacb53d509ac999
sammyredha <sammy.mounsif@melroseschools.com>
2022-01-26 19:22:24 -0500

    Ensured all code is using the correct zero :)

--- src/main/java/frc/robot/commands/DeployIntake.java
+++ src/main/java/frc/robot/commands/DeployIntake.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.Constants;
 import frc.robot.subsystems.IntakeFourBar;
 import frc.robot.subsystems.IntakeSubsystem;
 
@@ -9,8 +10,8 @@ public class DeployIntake extends ParallelCommandGroup {
 
     public DeployIntake(IntakeSubsystem intakeSubsystem, IntakeFourBar intakeFourBar) {
         addCommands(
-            new IntakeSetFourBar(intakeFourBar, 0), //we have no idea what the fully deployed position is supposed to be :(
-            new IntakeSetRollers(intakeSubsystem, 3) //no idea what a good speed is, test this please :)
+            new IntakeSetFourBar(intakeFourBar, Constants.zero), //we have no idea what the fully deployed position is supposed to be :(
+            new IntakeSetRollers(intakeSubsystem, Constants.IntakeConstants.speed)
         );
     }
 }
