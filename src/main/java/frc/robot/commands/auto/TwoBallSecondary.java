0d20e8357f9b07931379abbe8dd322dd01b440ce
sammyredha <sammy.mounsif@melroseschools.com>
2022-03-13 20:35:09 -0400

    Implemented and renamed a simple secondary auto

--- src/main/java/frc/robot/commands/auto/TwoBallSecondary.java
+++ src/main/java/frc/robot/commands/auto/TwoBallSecondary.java
@@ -1,10 +1,11 @@
 package frc.robot.commands.auto;
 
 import edu.wpi.first.math.trajectory.Trajectory;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.commands.DeployIntake;
 import frc.robot.commands.RamsetA;
-import frc.robot.commands.ShootTillEmpty;
+import frc.robot.commands.ShootEverything;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.IntakeFourBar;
 import frc.robot.subsystems.IntakeSubsystem;
@@ -14,23 +15,23 @@ import frc.robot.util.FieldConstants;
 import frc.robot.util.Util;
 import java.util.List;
 
-public class TwoBallSecondarySide extends SequentialCommandGroup {
+public class TwoBallSecondary extends SequentialCommandGroup {
 
   private static Trajectory leg1 =
       RamsetA.makeTrajectory(
           0, List.of(FieldConstants.StartingPoints.fenderA, FieldConstants.cargoB), 0, false);
 
-  public TwoBallSecondarySide(
+  public TwoBallSecondary(
       DriveSubsystem driveSubsystem,
       IntakeSubsystem intakeSubsystem,
       IntakeFourBar intakeFourBar,
       ShootSubsystem shootSubsystem,
       SnekSystem snekSystem) {
     addCommands(
-        sequence(
+        new ParallelRaceGroup(
             new DeployIntake(intakeSubsystem, intakeFourBar),
             RamsetA.RamseteSchmoove(leg1, driveSubsystem),
-            RamsetA.RamseteSchmoove(Util.invertTrajectory(leg1), driveSubsystem),
-            new ShootTillEmpty(shootSubsystem, snekSystem)));
+            RamsetA.RamseteSchmoove(Util.invertTrajectory(leg1), driveSubsystem)),
+        new ShootEverything(snekSystem, shootSubsystem));
   }
 }
