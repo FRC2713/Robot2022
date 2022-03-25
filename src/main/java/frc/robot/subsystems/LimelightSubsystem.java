c99b74f9bbb7d36687046ca1d68d2c015d8f1376
bmccarron1 <Brigid.mccarron@melroseschools.com>
2022-03-24 20:21:24 -0400

    Adding distance code to limelight

--- src/main/java/frc/robot/subsystems/LimelightSubsystem.java
+++ src/main/java/frc/robot/subsystems/LimelightSubsystem.java
@@ -6,7 +6,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LimelightSubsystem extends SubsystemBase {
-
+  private double limelightHeightInches;
   private NetworkTable getTable() {
     return NetworkTableInstance.getDefault().getTable("limelight");
   }
@@ -127,6 +127,28 @@ public class LimelightSubsystem extends SubsystemBase {
     }
   }
 
+  public double getDistanceFromGoal(){
+    NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+  NetworkTableEntry ty = table.getEntry("ty");
+  double targetOffsetAngle_Vertical = ty.getDouble(0.0);
+
+  // how many degrees back is your limelight rotated from perfectly vertical?
+  //CHECK
+  double limelightMountAngleDegrees = 25.0;
+
+  // distance from the center of the Limelight lens to the floor
+  //CHANGE how high up it is
+  double limelightLensHeightInches = 20.0;
+
+  // distance from the target to the floor
+  double goalHeightInches = 104.0;
+
+  double angleToGoalDegrees = limelightMountAngleDegrees + targetOffsetAngle_Vertical;
+  double angleToGoalRadians = angleToGoalDegrees * (3.14159 / 180.0);
+
+  //calculate distance
+  return (goalHeightInches - limelightHeightInches)/Math.tan(angleToGoalRadians);
+  }
 //   public enum CamMode {
 //     VISION_CAM(0),
 //     DRIVER_CAM(1),
